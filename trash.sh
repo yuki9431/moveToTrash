d5d9b6233fd3bfb61d7288fe990f085fc5f4157e
yuki9431 <yuki9431@gmail.com>
2020-06-08 13:17:04 +0900

    fix add error handling

--- trash.sh
+++ trash.sh
@@ -2,10 +2,15 @@
 
 TRASH=/Users/${USER}/.Trash
 
+if [ ${#} -eq 0 ]; then
+    echo "ERROR: Nothing arguments."
+    exit 1
+fi
+
 for arg; do
     if [ ! -e ${TRASH}/${arg} ]; then
         mv -v ${arg} ${TRASH}/
     else
-        mv -v ${arg} ${TRASH}/${arg}_$(date "+%Y%m%d-%H%M%S")
+        mv -v ${arg} ${TRASH}/${arg}_$(date "+%Y%m%d_%H%M%S")
     fi
 done
